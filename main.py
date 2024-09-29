2fe1a91fc598ae0e214f15132741b883ecde4865
Simran Singh <m.simx2909@gmail.com>
2024-09-29 23:10:49 +0530

    Added minor functionalities

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 from flask import Flask, render_template, request, jsonify
 import json
-import re
 
 app = Flask(__name__)
 
-with open('Dic_test.json', 'r', encoding='utf-8') as f:
-    dictionary = json.load(f)
+def load_dictionary(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+dictionary1 = load_dictionary('dic-1.json')
+dictionary2 = load_dictionary('dic-2.json')
+dictionary3 = load_dictionary('dic-3.json')
 
 def hyperlink_words(meaning):
-    words_in_meaning = meaning.split()
-    for i, word in enumerate(words_in_meaning):
-        clean_word = word.lower()
-        if clean_word in dictionary:
-            words_in_meaning[i] = f'<a href="#" class="linked-word" data-word="{clean_word}">{word}</a>'
-    return ' '.join(words_in_meaning)
+    if meaning:
+        words_in_meaning = meaning.split()
+        for i, word in enumerate(words_in_meaning):
+            clean_word = word.lower()
+            if clean_word in dictionary1 or clean_word in dictionary2 or clean_word in dictionary3:
+                words_in_meaning[i] = f'<a href="#" class="linked-word" data-word="{clean_word}">{word}</a>'
+        return ' '.join(words_in_meaning)
+    return 'Meaning not found'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/lookup', methods=['POST'])
 def lookup():
-    word = request.form['word'].strip().lower()
-    meaning = dictionary.get(word)
-    if meaning:
-        meaning_with_links = hyperlink_words(meaning)
-        return jsonify({'word': word, 'meaning': meaning_with_links})
-    else:
-        return jsonify({'word': word, 'meaning': 'Word not found in the dictionary.'})
+    word = request.json.get('word').strip().lower()
+    meaning1 = dictionary1.get(word, None)
+    meaning2 = dictionary2.get(word, None)
+    meaning3 = dictionary3.get(word, None)
+
+    meanings = {
+        'dictionary1': meaning1,
+        'dictionary2': meaning2,
+        'dictionary3': meaning3
+    }
+
+    meanings_with_links = {key: hyperlink_words(value) for key, value in meanings.items() if value}
+
+    if not any(meanings_with_links.values()):
+        return jsonify({'word': word, 'meanings': {}})
+
+    return jsonify({'word': word, 'meanings': meanings_with_links})
+
 
 @app.route('/suggest', methods=['GET'])
 def suggest():
     query = request.args.get('query').lower()
-    suggestions = [word for word in dictionary.keys() if word.startswith(query)]
-    return jsonify(suggestions)
+    suggestions = set()
+
+    for dictionary in [dictionary1, dictionary2, dictionary3]:
+        suggestions.update([word for word in dictionary.keys() if word.startswith(query)])
+
+    return jsonify(list(suggestions))
 
 if __name__ == '__main__':
     app.run(debug=True)
